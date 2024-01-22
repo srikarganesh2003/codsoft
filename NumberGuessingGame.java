000c1f0b543d94421eaa44c428cdcc7b8facf309
srikarganesh2003 <104423696+srikarganesh2003@users.noreply.github.com>
2024-01-22 15:39:04 +0530

    Add files via upload

---
+++ NumberGuessingGame.java
@@ -0,0 +1,51 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGuessingGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int lowerBound = 1;
+        int upperBound = 100;
+        int attemptsLimit = 10;
+        int score = 0;
+
+        System.out.println("Welcome to the Number Guessing Game!");
+
+        do {
+            int targetNumber = random.nextInt(upperBound - lowerBound + 1) + lowerBound;
+            int attempts = 0;
+            boolean guessedCorrectly = false;
+
+            System.out.println("\nI have selected a number between " + lowerBound + " and " + upperBound + ". Try to guess!");
+
+            while (attempts < attemptsLimit) {
+                System.out.print("Enter your guess: ");
+                int userGuess = scanner.nextInt();
+                attempts++;
+
+                if (userGuess == targetNumber) {
+                    guessedCorrectly = true;
+                    break;
+                } else if (userGuess < targetNumber) {
+                    System.out.println("Too low! Try again.");
+                } else {
+                    System.out.println("Too high! Try again.");
+                }
+            }
+
+            if (guessedCorrectly) {
+                System.out.println("Congratulations! You guessed the correct number in " + attempts + " attempts.");
+                score++;
+            } else {
+                System.out.println("Sorry, you've reached the maximum number of attempts. The correct number was " + targetNumber + ".");
+            }
+
+            System.out.print("\nDo you want to play again? (yes/no): ");
+        } while (scanner.next().equalsIgnoreCase("yes"));
+
+        System.out.println("Game Over! Your total score is: " + score);
+        scanner.close();
+    }
+}
